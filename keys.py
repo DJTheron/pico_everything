a60d7a630fcff978dcca5448eb15e848f26c3968
Daniel <danieltheron11@proton.me>
2026-05-18 18:26:38 +0200

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,12 @@
+from machine import Pin
+
+keyA = Pin(15,Pin.IN,Pin.PULL_UP)
+keyB = Pin(17,Pin.IN,Pin.PULL_UP)
+keyX = Pin(19 ,Pin.IN,Pin.PULL_UP)
+keyY= Pin(21 ,Pin.IN,Pin.PULL_UP)
+
+up = Pin(2,Pin.IN,Pin.PULL_UP)
+down = Pin(18,Pin.IN,Pin.PULL_UP)
+left = Pin(16,Pin.IN,Pin.PULL_UP)
+right = Pin(20,Pin.IN,Pin.PULL_UP)
+ctrl = Pin(3,Pin.IN,Pin.PULL_UP)

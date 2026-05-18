ce1e1a60bdd72ded951433abae30ea083fdf3c18
Daniel <danieltheron11@proton.me>
2026-05-18 16:16:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-import LCD_Lib
-
-LCD = LCD_Lib.LCD_1inch3()
+from LCD_Lib import LCD
+import os
 
 def main():
-    LCD_Lib.setup()
     LCD.fill(0x0000)
     LCD.show()
 
 def displaycard(title):
     LCD.text(title, 70, 115, LCD.white)
-
+    
 if __name__=='__main__':
     main()
     displaycard("Hello World")

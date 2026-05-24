a707cfde7c3dcb6bdcfaacda2d5f72e2bde004df
Daniel <danieltheron11@proton.me>
2026-05-24 16:36:47 +0200

    Update Calculator.py

--- apps/Calculator.py
+++ apps/Calculator.py
@@ -6,3 +6,43 @@ active_grid = [[0, 0, 0, 0],
                [0, 0, 0, 0],
                [0, 0, 0, 0],   
                [0, 0, 0, 0]]
+
+x = 0
+y = 0
+
+while True:
+    if up.value() == 0:
+        if y == 4:
+            continue
+        else:
+            y += 1
+        while up.value() == 0:
+            pass
+        time.sleep(0.05)    
+
+    if down.value() == 0:
+            if y == 0:
+                continue
+            else:
+                y -= 1
+            while down.value() == 0:
+                pass
+            time.sleep(0.05)  
+            
+    if left.value() == 0:
+        if x == 0:
+            continue
+        else:
+            x -= 1
+        while left.value() == 0:
+            pass
+        time.sleep(0.05)  
+        
+    if right.value() == 0:
+        if x == 4:
+            continue
+        else:
+            x += 1
+        while right.value() == 0:
+            pass
+        time.sleep(0.05)

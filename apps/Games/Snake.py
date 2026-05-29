0d6bc27cfd21048ab3b50cc331baa007c8ffaae8
TheGuyInTheRedSuit <kylelsturrock@gmail.com>
2026-05-29 16:58:30 +0200

    ee

--- apps/Games/Snake.py
+++ apps/Games/Snake.py
@@ -8,21 +8,26 @@ def run():
     snake = [[2,7],[1,7],[0,7]]
     dir = [1,0]
 
-    # input
-    if up.value() == 0:
-        dir = [0,-1]
-    elif down.value() == 0:
-        dir = [0,1]
-    elif right.value() == 0:
-        dir = [1,0]
-    elif left.value() == 0:
-        dir = [-1,0]
 
-    # fysics
+    while True:
+        # input
+        if up.value() == 0:
+            dir = [0,-1]
+        elif down.value() == 0:
+            dir = [0,1]
+        elif right.value() == 0:
+            dir = [1,0]
+        elif left.value() == 0:
+            dir = [-1,0]
 
-    # move snake
-    for i in range(snake):
-        snake[i][0] += dir[0]
-        snake[i][1] += dir[1]
+        if keyY.value() == 0:
+            return
 
-    # drawing
+        # fysics
+
+        # move snake
+        for i in range(len(snake)):
+            snake[i][0] += dir[0]
+            snake[i][1] += dir[1]
+
+        # drawing

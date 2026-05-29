b5cc049e3db9e666abaa81827cce642eb50ec179
Daniel <danieltheron11@proton.me>
2026-05-29 16:56:37 +0200

    Update acconting.py

--- acconting.py
+++ acconting.py
@@ -1,8 +1,8 @@
-prices = "250,450,1700,35.40"
+prices = "330,176,1144,1738"
 markup = 120
 
 for price in prices.split(","):
     costprice = float(price) * (100/(100+markup))
-    grossprofit = float(price) - costprice
+    grossprofit = float(price) - costprice # add proper want/have calc
     
     print("Cost Price:", costprice, "|", "Gross Profit:", grossprofit)
